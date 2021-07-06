62d07df4297f01ed750d56ebe1b6b33c02795215
Gu-Santos <82477118+Gu-Santos@users.noreply.github.com>
2021-07-06 15:56:53 -0300

    Add files via upload

---
+++ exfixacao/maioremenornum/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace maioremenornum
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numeros = new int[10];
+            int maior_numero;
+            int menor_numero;
+
+
+            for (int contador = 0; contador < 10; contador++)
+            {
+                Console.WriteLine("Escreva um número: ");
+                numeros[contador] = int.Parse(Console.ReadLine());
+            }
+
+            maior_numero = numeros[0];
+            menor_numero = numeros[0];
+
+            for (int i = 0; i < 10; i++)
+            {
+                if (numeros[i] < menor_numero)
+                {
+                    menor_numero = numeros[i];
+                }
+                
+                if (numeros[i] > maior_numero)
+                {
+                    maior_numero = numeros[i];
+                }
+                    
+            }
+
+
+            Console.WriteLine("O maior valor digitado foi: " + maior_numero);
+
+            Console.WriteLine("O menor valor digitado foi: " + menor_numero);
+
+        }
+    }
+}
---
+++ exfixacao/tabuada1a10/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace tabuada1a10
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+           for (int numeros_tabuada = 1; numeros_tabuada < 11; numeros_tabuada++)
+           {
+               for (int numeros_multiplicados = 1; numeros_multiplicados < 11; numeros_multiplicados++)
+               {
+                Console.WriteLine(numeros_tabuada*numeros_multiplicados);
+               }
+           }
+        }
+    }
+}
